567e0911f1e2308f40e316b4d7c2a8dec09dddca
romkravets <romann.kravets@gmail.com>
2020-08-23 22:50:55 +0300

    fetch data

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './UI/Button/Button.jsx';
 export { default as Header } from './Header/Header.jsx'
 export { default as Categories } from './Categories/Categories.jsx'
 export { default as SortPopup } from './SortPopup/SortPopup.jsx'
+export { default as PizzaBlock } from './PizzaBlock/PizzaBlock.jsx'
